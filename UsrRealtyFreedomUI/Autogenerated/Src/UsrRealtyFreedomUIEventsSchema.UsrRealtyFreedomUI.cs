8dfba2d02a3b16752eae6986f181ea8e6f23d65c
Bernat Bosca Candel <bernat@neotic.es>
2024-01-31 19:36:58 +0100

    At this point works

---
+++ UsrRealtyFreedomUI/Autogenerated/Src/UsrRealtyFreedomUIEventsSchema.UsrRealtyFreedomUI.cs
@@ -0,0 +1,57 @@
+﻿namespace Terrasoft.Configuration
+{
+
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Globalization;
+	using Terrasoft.Common;
+	using Terrasoft.Core;
+	using Terrasoft.Core.Configuration;
+
+	#region Class: UsrRealtyFreedomUIEventsSchema
+
+	/// <exclude/>
+	public class UsrRealtyFreedomUIEventsSchema : Terrasoft.Core.SourceCodeSchema
+	{
+
+		#region Constructors: Public
+
+		public UsrRealtyFreedomUIEventsSchema(SourceCodeSchemaManager sourceCodeSchemaManager)
+			: base(sourceCodeSchemaManager) {
+		}
+
+		public UsrRealtyFreedomUIEventsSchema(UsrRealtyFreedomUIEventsSchema source)
+			: base( source) {
+		}
+
+		#endregion
+
+		#region Methods: Protected
+
+		protected override void InitializeProperties() {
+			base.InitializeProperties();
+			UId = new Guid("c9ba3998-4f6f-41a8-8253-c26e0de611fe");
+			Name = "UsrRealtyFreedomUIEvents";
+			ParentSchemaUId = new Guid("50e3acc0-26fc-4237-a095-849a1d534bd3");
+			CreatedInPackageId = new Guid("ecb97a1b-24c8-40ec-b644-07e3c8d131b7");
+			ZipBody = new byte[] { 31,139,8,0,0,0,0,0,4,0,83,208,211,211,3,0,43,31,116,100,4,0,0,0 };
+		}
+
+		#endregion
+
+		#region Methods: Public
+
+		public override void GetParentRealUIds(Collection<Guid> realUIds) {
+			base.GetParentRealUIds(realUIds);
+			realUIds.Add(new Guid("c9ba3998-4f6f-41a8-8253-c26e0de611fe"));
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
+
--- UsrRealtyFreedomUI/Schemas/UsrMyUtils/UsrMyUtils.js
+++ UsrRealtyFreedomUI/Schemas/UsrMyUtils/UsrMyUtils.js
@@ -1,4 +1,4 @@
- define("UsrMyUtils", [], function() {
+ define("UsrMyUtils", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function /**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
 	return {
 		version: "1.0", 
 		my_proc: function(a, b){
